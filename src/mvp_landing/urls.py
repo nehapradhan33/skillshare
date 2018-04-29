338ada11debeacc727bdcdc4d63c64440c62609c
nehapradhan33 <kevneha@gmail.com>
2018-04-29 17:57:45 +0545

    first commit

---
+++ src/mvp_landing/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import *
+from django.contrib import admin
+admin.autodiscover()
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'mvp_landing.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+]
